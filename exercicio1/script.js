8f2e6c3eafd723e6ca3459f45e5f656faa9acee9
imlaerciojunior <imlaerciojunior@gmail.com>
2023-02-13 22:38:36 -0300

    Resultados Exercicios Array

---
+++ exercicio1/script.js
@@ -0,0 +1,17 @@
+// EXERCÍCIO I
+
+//a))
+
+const numerosVariados = [11,24,3,2023,13]
+
+//b)
+
+const apenasStrings = ["Biju", "Shoyo", "garrafa", "Mozão"]
+
+//c)
+
+const arrayVariado = [11, "musica", false]
+
+console.log(numerosVariados)
+console.log(apenasStrings)
+console.log(arrayVariado)
